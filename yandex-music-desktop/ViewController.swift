fd883df6fce605f681205c7cea49ee1f4d6197ea
code2319 <28976238+code2319@users.noreply.github.com>
2023-03-11 11:57:18 +0300

    Initial Commit

---
+++ yandex-music-desktop/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  yandex-music-desktop
+//
+//  Created by user on 11.03.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
